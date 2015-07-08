680ae9f19ceb61c2c7e5d808ca71a248f1fe01ad
hcnode <7372@163.com>
2015-07-08 15:04:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var program = require('commander');
+var request = require('request');
+var fs = require('fs');
+
+program
+  .version('0.0.1')
+  // .option('-a, --add', 'Add .gitignore')
+  .parse(process.argv);
+
+var ws = fs.createWriteStream('.gitignore');
+ws.on("finish", function(){
+	console.log("done.");
+});
+request
+  .get('https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore')
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .pipe(ws);
+
