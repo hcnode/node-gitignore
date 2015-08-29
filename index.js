994b1348a97248bfdeb52e02c4dc0c1f8bd76be9
hcnode <7372@163.com>
2015-08-29 10:45:37 +0800

    update index

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ program
   .version('0.0.1')
   // .option('-a, --add', 'Add .gitignore')
   .parse(process.argv);
-
+var append = "# .idea\n.idea\n# Dependency directory\nbower_components";
 var ws = fs.createWriteStream('.gitignore');
 ws.on("finish", function(){
-	console.log("done.");
+	fs.appendFile(".gitignore", append, function (err, data) {
+		if(err){
+			console.log("error.");
+		}else {
+			console.log("done.");
+		}
+	});
 });
 request
-  .get('https://raw.githubusercontent.com/hcnode/node-gitignore/master/.gitignore')
+  .get('https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore')
   .on('error', function(err) {
     console.log(err)
   })
