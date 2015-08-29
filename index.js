4d902083b2322917243f9e68d3789471b7054e35
hcnode <7372@163.com>
2015-08-29 20:20:49 +0800

    update index

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var fs = require('fs');
 
 program
   .version('0.0.1')
-  // .option('-a, --add', 'Add .gitignore')
   .parse(process.argv);
+
 var append = "# .idea\n.idea\n# Dependency directory\nbower_components";
+
 var ws = fs.createWriteStream('.gitignore');
 ws.on("finish", function(){
 	fs.appendFile(".gitignore", append, function (err, data) {
+
 		if(err){
 			console.log("error.");
 		}else {
